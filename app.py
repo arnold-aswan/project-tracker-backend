633eb73aaf15b99b952fe0a70c057bba379bf3a8
user <ephrahim.lenaiayasa@student.moringaschool.com>
2023-10-27 02:50:35 +0300

    Added ProjectsResource,ProjectByIdResource and ClassResource,

--- app.py
+++ app.py
@@ -1,34 +1,104 @@
-from flask import Flask
+from flask import Flask, request, make_response, jsonify
 from flask_migrate import Migrate
-from models import db, User, Project, Admins, Class, ProjectMember
-from flask_restful import Resource, Api
+from models import db, User, Project, Class , project_members, Admin
+from flask_restful import Resource, Api, marshal_with, fields
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project-tracker.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
-migrate = Migrate(app,db)
-
-
+migrate = Migrate(app, db)
 
 api = Api(app)
 
 db.init_app(app)
 
-class Projects(Resource):
+
+class ProjectsResource(Resource):
+    @marshal_with({'id': fields.Integer, 'name': fields.String, 'description': fields.String, 'users': fields.List(fields.String)})
+
 
     def get(self):
-        projects = [project.to_dict() for project in Project.query.all()]
+        # Query the database to get all projects and their associated users
+        projects = Project.query.all()
 
-        response_dict = {
-            "projects": projects  
-        }
+        # Prepare the response data
+        result = []
+        for project in projects:
+            users = [user.username for user in project.project_users]
+            project_data = {
+                'id': project.id,
+                'name': project.name,
+                'description': project.description,
+                'users': users
+            }
+            result.append(project_data)
 
-        return response_dict, 200
+        return result
+    def post(self):
 
-api.add_resource(Projects, '/projects')
+        data = request.form
 
+        required_fields = ['name','description', 'github_link','user_id','class_id']
+        for field in required_fields:
+            if field not in data:
+                return {"error": f"'{field}' is required"}, 400
+            
+        new_project = Project(
+            name=data['name'],
+            description=data['description'],
+            github_link=data['github_link'],
+            user_id=data['user_id'],
+            class_id=data['class_id']
+        )
 
+        db.session.add(new_project)
+        db.session.commit()
 
+        response_dict = new_project.to_dict()
+        return response_dict,201
+class ProjectByIdResource(Resource):
+    def delete(self, id):
+        project = Project.query.get(id)
+        if project:
+            db.session.delete(project)
+            db.session.commit()
+            return {"message": "Project deleted successfully"}, 200
+        return {"message": "Project not found"}, 404
+
+class ClassResource(Resource):
+    def get(self):
+        response_dict = [n.to_dict() for n in Class.query.all()]
+
+        response = make_response(
+            jsonify(response_dict),
+            200
+        )
+
+        return response
+    
+    def post(self):
+        data = request.form
+
+        required_fields = ['name','user_id','admin_id']
+        for field in required_fields:
+            if field not in data:
+                return {"error": f"'{field}' is required"}, 400
+            
+        new_class = Class(
+            name=data['name'],
+            user_id=data['user_id'],
+            admin_id=data['admin_id']
+        )
+
+        db.session.add(new_class)
+        db.session.commit()
+
+        response_dict = new_class.to_dict()
+        return response_dict,201
+    
+api.add_resource(ClassResource, '/classes')
+api.add_resource(ProjectByIdResource, '/project/<int:id>')
+api.add_resource(ProjectsResource, '/projects')
 if __name__ == '__main__':
     app.run(port=5555)
--- seed.py
+++ seed.py
@@ -1,36 +1,76 @@
-from app import app, db, User, Project, Admins, Class, ProjectMember
 from faker import Faker
-import random
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from models import User, Admin, Class, Project, db
+
+app = Flask(__name__)
+
+# Update the app configuration with your database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project-tracker.db'
+db.init_app(app)
+
+# Create tables in the database
+with app.app_context():
+    db.create_all()
 
 fake = Faker()
 
-def generate_fake_data():
-    num_users = 10
-    num_projects = 20
-    num_classes = 5  # Adjust this number as needed
+# Function to create users
+def create_users(num_users):
+    for _ in range(num_users):
+        user = User(
+            username=fake.user_name(),
+            email=fake.email(),
+            password='your_hashed_password_here'
+        )
+        db.session.add(user)
+    db.session.commit()
 
-    with app.app_context():
-        # Generate fake users
-        for _ in range(num_users):
-            user = User(
-                username=fake.user_name(),
-                email=fake.email(),
-                password_hash='your_password_hash_here'  # Replace with hashed password
-            )
-            db.session.add(user)
+# Function to create admins
+# Function to create admins
+def create_admins(num_admins):
+    for _ in range(num_admins):
+        # Create a new user for the admin
+        user = User(
+            username=fake.user_name(),
+            email=fake.email(),
+            password='your_hashed_password_here'
+        )
+        db.session.add(user)
+        db.session.commit()  # Commit the user to get the user_id
 
-        # Generate fake projects
-        for _ in range(num_projects):
-            project = Project(
-                name=fake.word(),
-                description=fake.sentence(),
-                admin_id=random.randint(1, num_users),  # Randomly assign an admin
-                github_link=fake.url(),
-                class_id=random.randint(1, num_classes),  # Randomly assign a class
-            )
-            db.session.add(project)
+        # Create the admin using the user_id
+        admin = Admin(user_id=user.id)
+        db.session.add(admin)
 
-        db.session.commit()
+    db.session.commit()
+
+
+# Function to create classes
+def create_classes(num_classes):
+    for _ in range(num_classes):
+        class_obj = Class(
+            name=fake.word()
+        )
+        db.session.add(class_obj)
+    db.session.commit()
+
+# Function to create projects
+def create_projects(num_projects, num_users, num_classes):
+    for _ in range(num_projects):
+        project = Project(
+            name=fake.sentence(nb_words=3),
+            description=fake.paragraph(nb_sentences=3),
+            github_link=fake.url(),
+            user_id=fake.random_int(min=1, max=num_users),
+            class_id=fake.random_int(min=1, max=num_classes)
+        )
+        db.session.add(project)
+    db.session.commit()
 
 if __name__ == '__main__':
-    generate_fake_data()
+    with app.app_context():
+        create_users(num_users=10)  # Adjust the number of users as needed
+        create_admins(num_admins=3)  # Adjust the number of admin users as needed
+        create_classes(num_classes=5)  # Adjust the number of classes as needed
+        create_projects(num_projects=20, num_users=10, num_classes=5)  # Adjust the number of projects as needed
