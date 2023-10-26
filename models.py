8b28235ea2fa6f6c4beabecfd19cfe8bdf2b6885
user <ephrahim.lenaiayasa@student.moringaschool.com>
2023-10-26 08:12:22 +0300

    Created models

---
+++ models.py
@@ -0,0 +1,67 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+
+db = SQLAlchemy()
+
+# Define the association table for the many-to-many relationship between users and projects using project members table
+class ProjectMember(db.Model):
+    __tablename__ = 'project_members'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    project_id = db.Column(db.Integer, db.ForeignKey('projects.id'))
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    #Many to many relationshio with the user and projects
+    projects = db.relationship('Project', secondary=ProjectMember, back_populates='members')
+
+class Project(db.Model):
+    __tablename__ = 'projects'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(80), nullable=False)
+    admin_id = db.Column(db.Integer, db.ForeignKey('admins.id'))
+    github_link = db.Column(db.String(80), nullable=False)
+    class_id = db.Column(db.Integer, db.ForeignKey('classes.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    #Many-to-one relationship with admins
+    admin = db.relationship('Admins', back_populates='projects')
+
+    #Many-to-one relationship with classes
+    project_class = db.relationship('Class', back_populates='projects')
+
+    #Many-to-many relationship with users
+    members = db.relationship('User', secondary=ProjectMember, back_populates='projects')
+
+class Admins(db.Model):
+    __tablename__ = 'admins'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    #One-to-many relationship with projects
+    projects = db.relationship('Project', back_populates='admin')
+
+class Class(db.Model):
+    __tablename__ = 'classes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(80), nullable=False)
+
+    #One-to-many relationship with projects
+    projects = db.relationship('Project', back_populates='project_class')
