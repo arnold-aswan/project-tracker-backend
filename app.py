fa64be81cd9139d20c44b56c3ef55a2a71794501
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-10-27 09:04:15 +0300

    added cohort route

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import Flask
 from flask_migrate import Migrate
 from models import db, User, Project, Admins, Class, ProjectMember
 from flask_restful import Resource, Api
+from flask_cors import CORS  # pip install flask-cors
+
 app = Flask(__name__)
+CORS(app)   # for cross origin 
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project-tracker.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
@@ -25,9 +29,20 @@ class Projects(Resource):
         }
 
         return response_dict, 200
+    
+# Classes route
+class Cohort(Resource):
+    def get(self):
+        cohorts = [cohort.to_dict() for cohort in Class.query.all()]
+        
+        response_dict = {
+            "cohorts" : cohorts
+        }    
+        
+        return response_dict, 200    
 
 api.add_resource(Projects, '/projects')
-
+api.add_resource(Cohort, "/class")
 
 
 if __name__ == '__main__':
