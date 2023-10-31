7874b792f6c1defe803605c2c494eede8277774a
TracyAntonia <aantonjuguna@gmail.com>
2023-10-31 09:58:41 +0300

    added the roles

--- models.py
+++ models.py
@@ -16,13 +16,12 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(80), nullable=False)
-    last_name = db.Column(db.String(80), nullable=False)
-    username = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    first_name = db.Column(db.String(80), nullable=True, default="Unknown")
+    last_name = db.Column(db.String(80), nullable=True, default="Unknown")
+    role = db.Column(db.String(80), nullable=True, default="Student")
 
     #Many to many relationshio with the user and projects
     projects = db.relationship('Project', secondary=ProjectMember.__table__, back_populates='users')
